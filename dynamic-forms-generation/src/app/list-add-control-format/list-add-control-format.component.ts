3fb4180db0f9ea5724696559ad7d4b10a9a19d28
ankitkumarsharma <aksharma.dixit@gmail.com>
2023-12-17 01:25:41 +0530

    list add control dynamic form- card crud added

--- dynamic-forms-generation/src/app/list-add-control-format/list-add-control-format.component.ts
+++ dynamic-forms-generation/src/app/list-add-control-format/list-add-control-format.component.ts
@@ -1,23 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CARD_LIST, LABELS } from '../core/app.constant';
 import { CardListModel } from '../core/app.models';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list-add-control-format',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './list-add-control-format.component.html',
   styleUrl: './list-add-control-format.component.scss'
 })
-export class ListAddControlFormatComponent {
+export class ListAddControlFormatComponent implements OnInit {
   cardList: CardListModel[] = CARD_LIST;
-  noDataFound:string = LABELS.noDataFound;
-  addCard(){
-    let obj = {
-      cardName: 'Another Card',
-      desc: 'Another desc 1',
-      controlsList: []
+  noDataFound: string = LABELS.noDataFound;
+  addCardFormGroup!: FormGroup;
+  addCardFormGroupSubmitted!: boolean;
+  cardEditIndex!: number;
+  cardEditFlag!: boolean;
+  ngOnInit(): void {
+    this.initAddCardFormGroup();
+  }
+  // card add/edit
+  initAddCardFormGroup() {
+    this.addCardFormGroup = new FormGroup({
+      name: new FormControl('', Validators.required),
+      desc: new FormControl('', Validators.required),
+    })
+  }
+  getCardNameRequiredError() {
+    return this.addCardFormGroupSubmitted && this.addCardFormGroup.controls['name'].hasError('required');
+  }
+  getCardNameError() {
+    return this.addCardFormGroupSubmitted && this.addCardFormGroup.controls['name'].errors;
+  }
+  getCardDescError() {
+    return this.addCardFormGroupSubmitted && this.addCardFormGroup.controls['desc'].errors;
+  }
+  getCardDescRequiredError() {
+    return this.addCardFormGroupSubmitted && this.addCardFormGroup.controls['desc'].hasError('required');
+  }
+  requiredErrorDescription(label: string) {
+    return `${label} field is required`;
+  }
+  resetAddCardFormGroup() {
+    this.addCardFormGroup.reset();
+    this.addCardFormGroupSubmitted = false;
+  }
+  addCard(value: any) {
+    this.addCardFormGroupSubmitted = true;
+    if (this.addCardFormGroup.valid) {
+      value = { ...value, controlsList: [] }
+      this.cardList = [...this.cardList, value];
+      this.resetAddCardFormGroup();
+    } else {
+      this.addCardFormGroup.markAllAsTouched();
     }
-    this.cardList = [...this.cardList, obj];
+  }
+  onCardEdit(index: number) {
+    this.cardEditFlag = true;
+    this.cardEditIndex = index;
+    let updateObj: any;
+    let data: any = this.cardList[index];
+    Object.keys(this.addCardFormGroup.value).map((itemParent) => {
+      Object.keys(this.cardList[index]).map((itemChild) => {
+        if (itemParent == itemChild) {
+          updateObj = { ...updateObj, [itemParent]: data[itemParent] }
+        }
+      })
+    })
+    this.addCardFormGroup.setValue(updateObj);
+  }
+  onCardUpdate() {
+    this.addCardFormGroupSubmitted = true;
+    if (this.addCardFormGroup.valid) {
+      this.cardList[this.cardEditIndex] = this.addCardFormGroup.value;
+      this.cardEditFlag = false;
+      this.resetAddCardFormGroup();
+    } else {
+      this.addCardFormGroup.markAllAsTouched();
+    }
+
+  }
+  onCardDelete(index: number) {
+    this.cardList.splice(index, 1);
   }
 }
