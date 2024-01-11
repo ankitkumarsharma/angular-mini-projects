7691dc8d189f20d176986751b4b6162c49132554
ankitkumarsharma <aksharma.dixit@gmail.com>
2024-01-11 22:50:03 +0530

    Dynamic Forms: Add/Delete buttons template functionality update

--- dynamic-forms-generation/src/app/dynamic-forms-with-array-format/dynamic-forms-with-array-format.component.ts
+++ dynamic-forms-generation/src/app/dynamic-forms-with-array-format/dynamic-forms-with-array-format.component.ts
@@ -15,20 +15,22 @@ export class DynamicFormsWithArrayFormatComponent implements OnInit {
   cardForm!: FormGroup;
 
   ngOnInit(): void {
+    // step 2
     this.initCardForm();
+    // step 7
     this.addCard();
   }
-
+  // step 1
   initCardForm() {
     this.cardForm = new FormGroup({
       cardList: new FormArray([])
     })
   }
-
+  // step 3
   getCardList() {
     return this.cardForm.get('cardList') as FormArray;
   }
-
+  // step 4
   addNewControls() {
     return new FormGroup({
       name: new FormControl(''),
@@ -36,37 +38,37 @@ export class DynamicFormsWithArrayFormatComponent implements OnInit {
       category: new FormControl(''),
     })
   }
-
+  // step 5
   getCard(): FormGroup {
     return new FormGroup({
       card: new FormArray([this.addNewControls()])
     })
   }
-
-  getCardGroup(i: number) {
-    return this.getCardList().controls[i] as FormGroup
-  }
-
-  getControlList(i: number) {
-    return this.getCardGroup(i).get('card') as FormArray;
-  }
-
-  addControls(i: number) {
-    this.getControlList(i).push(this.addNewControls());
-  }
-
-  removeControls(i: number, j: number) {
-    this.getControlList(i).removeAt(j);
-  }
-
+  // step 6
   addCard() {
     this.getCardList().push(this.getCard());
   }
-
+  // step 8
   removeCard(i: number) {
     this.getCardList().removeAt(i);
   }
-
+  // step 9
+  getCardGroup(i: number) {
+    return this.getCardList().controls[i] as FormGroup
+  }
+  // step 10
+  getControlList(i: number) {
+    return this.getCardGroup(i).get('card') as FormArray;
+  }
+  // step 11
+  addControls(i: number) {
+    this.getControlList(i).push(this.addNewControls());
+  }
+  // step 12
+  removeControls(i: number, j: number) {
+    this.getControlList(i).removeAt(j);
+  }
+  // step 13
   onSubmit() {
     console.log(this.cardForm.value)
   }
